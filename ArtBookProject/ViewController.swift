8697aabb20105bc4adcb27adaef92de722b67a50
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-03-03 20:31:24 +0300

    first commit

--- ArtBookProject/ViewController.swift
+++ ArtBookProject/ViewController.swift
@@ -6,14 +6,140 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    var selectedPainting = ""
+    var selectedPaintingId : UUID?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        getData()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    
+    @objc func getData() {
+        
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let result = try context.fetch(fetchRequest)
+            if result.count > 0 {
+                
+                for result in result as! [NSManagedObject]{
+                    if let name = result.value(forKey: "name") as? String{
+                        self.nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID{
+                        self.idArray.append(id)
+                    }
+                    self.tableView.reloadData()
+                }
+                
+            }
+            
+        }catch{
+            print("error")
+        }
+        
+    }
+    
+    
+    @objc func addButtonClicked(){
+        selectedPainting = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVc"{
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenPainting = selectedPainting
+            destinationVC.chosenPaintingId = selectedPaintingId
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)  {
+        if editingStyle == .delete {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let contex = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            let idString = idArray[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id= %@", idString)
+            
+            fetchRequest.returnsObjectsAsFaults = false
+            do {
+                let results = try contex.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey : "id") as? UUID{
+                            if id == idArray[indexPath.row]{
+                                contex.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                
+                                do {
+                                    try contex.save()
+                                } catch {
+                                    print("error")
+                                }
+                                
+                                break
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("error")
+            }
+            
+        }
+    }
 }
-
